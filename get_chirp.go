fdced45bd9418b6a5abb864097fa738ee904d01b
Shi Hong <shaquanhong@gmail.com>
2024-12-16 20:02:52 -0500

    newE: get singular chirp completed

---
+++ get_chirp.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func (cfg *apiConfig) handlerGetChirp(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Id        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserId    uuid.UUID `json:"user_id"`
+	}
+	chirpUUID, err := uuid.Parse(r.PathValue("chirpID"))
+	if err != nil {
+		respondWithError(w, 404, "Unable to Convert String to UUID", err)
+	}
+	res, err := cfg.db.GetChirp(r.Context(), chirpUUID)
+	if err != nil {
+		respondWithError(w, 404, "Unable to Get Chirp from UUID", err)
+	}
+	respondWithJSON(w, 200, chirp{
+		Id:        res.ID,
+		CreatedAt: res.CreatedAt,
+		UpdatedAt: res.UpdatedAt,
+		Body:      res.Body,
+		UserId:    res.UserID,
+	})
+}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -25,3 +25,7 @@ RETURNING *;
 -- name: GetChirps :many
 SELECT * FROM chirps
 ORDER BY created_at;
+
+-- name: GetChirp :one
+SELECT * FROM chirps
+WHERE chirps.id = $1;
